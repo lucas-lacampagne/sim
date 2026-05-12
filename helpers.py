a9089adc3333e2b12290dfab3c4c8e04d4809a6f
Lucas <lucaslacampagne@gmail.com>
2026-05-12 16:36:22 +0200

    added time decorator and logging

--- helpers.py
+++ helpers.py
@@ -1,4 +1,9 @@
 from collections import Counter
+import time
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
 
 def get_dict(nodes, nodes_pos, nodes_neg, pos_val, neg_val, null_val):
     out={node:null_val for node in nodes}
@@ -14,3 +19,13 @@ class Count(Counter):
         'The count of elements not in the Counter is one.'
         # Needed so that self[missing_item] does not raise KeyError
         return 1
+    
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()        
+        logging.info(f'{method.__name__} {(te - ts) * 1000 :.2f}')
+
+        return result    
+    return timed
