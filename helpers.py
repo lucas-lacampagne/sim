2b859403388cace6ea8130313f54c3c4c8dbd01e
Lucas <lucaslacampagne@gmail.com>
2026-05-29 16:01:22 +0200

    added range to car visions + stored paths + helper class + refacto

--- helpers.py
+++ helpers.py
@@ -21,35 +21,49 @@ class Count(Counter):
         'The count of elements not in the Counter is one.'
         # Needed so that self[missing_item] does not raise KeyError
         return 1
-    
+
 def timeit(method):
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
-        te = time.time()        
+        te = time.time()
         logging.info(f'{method.__name__} {(te - ts) * 1000 :.2f}')
 
-        return result    
+        return result
     return timed
 
-def get_all_shortest_paths(graph : nx.DiGraph, weight:str='weight'):
-    def map_id(d:dict|int, mapping_dict:dict):
+class rx_helper:
+    def __init__(self, graph : nx.DiGraph):
+        self.nx_graph = graph
+        self.rx_g:rx.PyDiGraph = rx.networkx_converter(graph, keep_attributes=True)
+        self.rx_to_nx={node_id : node['__networkx_node__']
+            for node_id, node in zip(self.rx_g.node_indices(), self.rx_g.nodes())
+        }
+        self.nx_to_rx = {v:k for k,v in self.rx_to_nx.items()}
+
+    def map_id(self, d:dict|int|list, mapping_dict:dict):
         if type(d)==int:
             return mapping_dict[d]
         elif type(d)==dict:
             return {mapping_dict[k]:[[mapping_dict[node] for node in v]] if v is not None else k for k,v in d.items()}
         elif type(d)==list:
-            return {mapping_dict[k]:[[mapping_dict[node] for node in v]] if v is not None else k for k,v in enumerate(d)}
+            return [mapping_dict[node] for node in d]
 
-    rx_g:rx.PyDiGraph = rx.networkx_converter(graph, keep_attributes=True)
-    rx_to_nx={}
-    for node_id, node in zip(rx_g.node_indices(), rx_g.nodes()):
-        rx_to_nx[node_id] = node['__networkx_node__']
-    # nx_to_rx = {v:k for k,v in rx_to_nx.items()}
+    def get_all_shortest_paths(self, weight:str='weight'):
+        rx_paths={k:dict(v) for k,v in dict(rx.all_pairs_dijkstra_shortest_paths(
+            self.rx_g,
+            edge_cost_fn=lambda x:x.get(weight))).items()
+            }
+        nx_rx_paths={self.map_id(k, self.rx_to_nx): self.map_id(v, self.rx_to_nx) for k,v in rx_paths.items()}
+        for node in nx_rx_paths.keys():
+            nx_rx_paths[node][node]=[[node]]
+        return nx_rx_paths
 
-    rx_paths={k:dict(v) for k,v in dict(rx.all_pairs_dijkstra_shortest_paths(
-        rx_g, 
-        edge_cost_fn=lambda x:x.get(weight))).items()
-        }
-    nx_rx_paths={map_id(k, rx_to_nx): map_id(v, rx_to_nx) for k,v in rx_paths.items()}
-    return nx_rx_paths
+    def get_shortest_path(self, nx_source, nx_target, weight='weight'):
+        path = rx.dijkstra_shortest_paths(self.rx_g, self.nx_to_rx[nx_source], self.nx_to_rx[nx_target],weight_fn=lambda x:x.get(weight))
+        # print(list(dict(path).values()))
+        # if not rx.has_path(self.rx_g, self.nx_to_rx[nx_source], self.nx_to_rx[nx_target]):
+        #     print('Ah bon ?!')
+        #     print(nx_source, nx_target)
+        #     print(self.nx_to_rx[nx_source], self.nx_to_rx[nx_target])
+        return self.map_id(list(list(dict(path).values())[0]), self.rx_to_nx)
